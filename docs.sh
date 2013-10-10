01094dd4729009bd9f886ff84de17323d30c5c4b
Andres Cabrera <andres@lenovo-W530>
2013-10-10 13:33:31 -0700

    Added docs.sh script

---
+++ docs.sh
@@ -0,0 +1,3 @@
+open AlloSystem/doc/www/doxy/html/index.html || (pushd AlloSystem/doc && doxygen Doxyfile && popd && open AlloSystem/doc/www/doxy/html/index.html)
+open Gamma/doc/html/index.html || (pushd Gamma/doc && doxygen Doxyfile && popd && open Gamma/doc/html/index.html)
+open GLV/doc/html/index.html || (pushd GLV/doc && doxygen Doxyfile && popd && open GLV/doc/html/index.html)
