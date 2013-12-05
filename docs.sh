0a15dbf5a5f9bb2083bf9749ec11d5cb78f1f0ce
matthewjameswright <matt@create.ucsb.edu>
2013-12-05 09:00:37 -0800

    Use xdg-open on linux systems

--- docs.sh
+++ docs.sh
@@ -1,3 +1,13 @@
-open AlloSystem/doc/www/doxy/html/index.html || (pushd AlloSystem/doc && doxygen Doxyfile && popd && open AlloSystem/doc/www/doxy/html/index.html)
-open Gamma/doc/html/index.html || (pushd Gamma/doc && doxygen Doxyfile && popd && open Gamma/doc/html/index.html)
-open GLV/doc/html/index.html || (pushd GLV/doc && doxygen Doxyfile && popd && open GLV/doc/html/index.html)
+#!/bin/sh
+
+which doxygen || echo You need to install doxygen. type \"brew install doxygen\" in the terminal.
+
+if [ `which apt-get 2>/dev/null` ]; then
+  xdg-open AlloSystem/doc/www/doxy/html/index.html || (pushd AlloSystem/doc && doxygen Doxyfile && popd && xdg-open AlloSystem/doc/www/doxy/html/index.html)
+  xdg-open Gamma/doc/html/index.html || (pushd Gamma/doc && doxygen Doxyfile && popd && xdg-open Gamma/doc/html/index.html)
+  xdg-open GLV/doc/html/index.html || (pushd GLV/doc && doxygen Doxyfile && popd && xdg-open GLV/doc/html/index.html)
+else
+  open AlloSystem/doc/www/doxy/html/index.html || (pushd AlloSystem/doc && doxygen Doxyfile && popd && open AlloSystem/doc/www/doxy/html/index.html)
+  open Gamma/doc/html/index.html || (pushd Gamma/doc && doxygen Doxyfile && popd && open Gamma/doc/html/index.html)
+  open GLV/doc/html/index.html || (pushd GLV/doc && doxygen Doxyfile && popd && open GLV/doc/html/index.html)
+fi
