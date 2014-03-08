c55eb7563a467c2da0fa61dc038c28ca6b3764e3
Andres Cabrera <andres@lenovo-W530>
2014-03-08 14:41:40 -0800

    Minor improvements to scripts

--- run.sh
+++ run.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 # Change this to suit your needs
-CMAKE_FLAGS="-DBUILD_ALLOGLV=1 -DBUILD_GLV=1 -DBUILD_VSR=0 -DBUILD_GAMMA=1"
+BUILD_ALLOGLV=1
+BUILD_GLV=1
+BUILD_VSR=0
+BUILD_GAMMA=1
 
 # ------------------------------------------------
 # You shouldn't need to touch the stuff below
+
+CMAKE_FLAGS="-DBUILD_ALLOGLV=${BUILD_ALLOGLV} -DBUILD_GLV=${BUILD_GLV} -DBUILD_VSR=${BUILD_VSR} -DBUILD_GAMMA=${BUILD_GAMMA}"
 
 if [ $# == 0 ]
 then
@@ -20,7 +25,6 @@ echo FILENAME: ${FILENAME}
 TARGET=${FILENAME//\//_}_run
 echo TARGET: ${TARGET}
 
-
 if [ -f $1 ]
 then
   TARGET_FLAG="-DBUILD_APP_FILE=$1 -DBUILD_DIR=0"
@@ -32,8 +36,5 @@ else
   exit 1
 fi
 
-#rm CMakeCache.txt
-#rm AlloSystem/CMakeCache.txt
-cmake . ${CMAKE_FLAGS} ${TARGET_FLAG} -DNO_EXAMPLES=1 -DNO_PROJECTS=1
-
+cmake . ${CMAKE_FLAGS} ${TARGET_FLAG} -DNO_EXAMPLES=1
 make $TARGET -j4 $*
