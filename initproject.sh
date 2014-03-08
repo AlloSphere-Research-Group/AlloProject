8b11e976200a9eb83712c2682efe8c8913fd4e2f
Andres Cabrera <andres@lenovo-W530>
2014-03-08 10:30:18 -0800

    Minor changes to scripts

--- initproject.sh
+++ initproject.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
-# get AlloSystem as a submodule
+
 git init
 
-# After git 1.8.4 you can use --depth 1 as an option to submodule add, in the meantime
-# see hack below
 git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
 git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
 git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
