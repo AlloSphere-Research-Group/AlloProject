50a2616c52ac914028aeeddbd47b2ba9a92cff5e
Andres Cabrera <andres@lenovo-W530>
2014-05-29 12:20:17 -0700

    Fixes to makexcode script

--- makexcode.sh
+++ makexcode.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 # Change this to suit your needs
-CMAKE_FLAGS="-DBUILD_ALLOGLV=1 -DBUILD_GLV=1 -DBUILD_VSR=0 -DBUILD_GAMMA=1"
+CMAKE_FLAGS="-DBUILD_ALLOUTIL=1 -DBUILD_ALLOGLV=1 -DBUILD_GLV=1 -DBUILD_VSR=0 -DBUILD_GAMMA=1"
 
 # ------------------------------------------------
 FILENAME=`echo $1 |cut -d'.' -f1 | sed -e "s|/|_|g"`
