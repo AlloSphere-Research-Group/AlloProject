b114ce4a88dcdf18f18f16bf5c70bf5f33552abe
Andres Cabrera <mantaraya36@gmail.com>
2014-11-12 10:31:01 -0800

    Get right branch for Gamma

--- initproject.sh
+++ initproject.sh
@@ -4,7 +4,7 @@ git init
 
 git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
 git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
-git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
+git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
 git clone --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
 #git submodule add https://github.com/AlloSphere-Research-Group/vsr.git AlloSystem
 
