62718fa032c56196811e6619e4e435b7aac8170a
Keehong Youn <younkeehong@gmail.com>
2016-02-02 18:50:18 -0800

    submodule branch initial commit

--- initproject.sh
+++ initproject.sh
@@ -2,10 +2,15 @@
 
 git init
 
-git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
-git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
-git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
-git clone --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
+git submodule add --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
+git submodule add --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
+git submodule add --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
+git submodule add --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
+
+# git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/AlloSystem.git AlloSystem
+# git clone -b cmake --depth 1 https://github.com/AlloSphere-Research-Group/GLV.git GLV
+# git clone -b devel --depth 1 https://github.com/AlloSphere-Research-Group/Gamma.git Gamma
+# git clone --depth 1 https://github.com/rbtsx/cuttlebone.git cuttlebone
 #git submodule add https://github.com/AlloSphere-Research-Group/vsr.git AlloSystem
 
 #git add -A
